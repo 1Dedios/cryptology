807c57235a14237dbe52f42d448e44e29dab046e
Dedios <dediosprogramming@gmail.com>
2025-05-23 01:55:25 -0400

    Feat: implemented ascii decrypter

--- ascii_decryption.py
+++ ascii_decryption.py
@@ -1,19 +1,52 @@
-'''
-Code = list of ascii values
+"""
+Decrypts ASCII values to their alphabetical equivalent.
 
-decoded_ascii = list of ascii values (also called ordinal numbers) converted to their char equivalent
+Args:
+    ascii_val (list): ascii values in list form
 
-print = a string with the decoded_ascii values
+Returns:
+    Human readable, ASCII decrypted message.
+"""
 
-- chr() converts arguments in ascii to their character equivalent
-- ord() does the opposite of chr()
 
-'''
+def ascii_decrypter(ascii_val):
+    message = ""
+    for val in ascii_val:
+        message += chr(val)
+    return message
 
-code = [99, 114, 121, 112, 116, 111, 123, 65, 83, 67, 73, 73, 95, 112, 114, 49, 110, 116, 52, 98, 108, 51, 125]
-decoded_ascii = []
 
-for i in range(len(code)):
-    decoded_ascii.append(chr(code[i]))
+def main():
 
-print(''.join(decoded_ascii))
+    code = [
+        99,
+        114,
+        121,
+        112,
+        116,
+        111,
+        123,
+        65,
+        83,
+        67,
+        73,
+        73,
+        95,
+        112,
+        114,
+        49,
+        110,
+        116,
+        52,
+        98,
+        108,
+        51,
+        125,
+    ]
+
+    message = ascii_decrypter(code)
+    print(message)
+
+
+if __name__ == "__main__":
+    main()
